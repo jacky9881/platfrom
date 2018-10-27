807e6cd5de159e0accf1831ff454dcf69ef7b772
jacky9881 <jacky9881@163.com>
2018-10-27 16:34:39 +0800

    Update kafkaflume.sh

--- kafkaflume.sh
+++ kafkaflume.sh
@@ -1,13 +1,21 @@
 
 #启动Zookeeper
 /opt/kafka_2.11-2.0.0/bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
+
 #启动Kafka
 /opt/kafka_2.11-2.0.0/bin/kafka-server-start.sh -daemon config/server.properties
+
 #创建Kafka Topic
 /opt/kafka_2.11-2.0.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic testTopic 
+#查看已经创建的topic列表
+#/opt/kafka_2.11-2.0.0/bin/kafka-topics.sh --list --zookeeper localhost:2181
+# 查看此topic的属性
+#kafka-topics.sh --describe --zookeeper localhost:2181 --topic testTopic
+
 #启动消费Topic
 ＃/opt/kafka_2.11-2.0.0/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic testTopic --from-beginning
 /opt/kafka_2.11-2.0.0/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic testTopic --from-beginning
+
 #启动Flume
 /opt/apache-flume-1.8.0-bin/bin/flume-ng agent -n docker -c conf -f conf/kafka-example.conf -Dflume.root.logger=INFO,console
 
